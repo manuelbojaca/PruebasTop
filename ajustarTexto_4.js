36648517e53c3c1d4877ecda3171c55f003ed92d
manueliciouss <manuel.bojaca@outlook.com>
2022-02-25 09:27:30 -0500

    completados primeros 4 ejercicios bucles forEach sin medir tiempo

---
+++ ajustarTexto_4.js
@@ -0,0 +1,14 @@
+function ajustarTexto (str, len) {
+    if (len > str.length){
+        let n = (str.length === 0) ? 1 : len - str.length;
+        for (let i = 0; i < n; i++){
+            str = str + '1';
+        }
+    }
+    return str.substr(0, len);
+}
+
+console.log(ajustarTexto("", 3)) // " " 
+console.log(ajustarTexto("hola", 2)) // "ho" 
+console.log(ajustarTexto("Hola", 0)) // "" 
+console.log(ajustarTexto("Hola", 5)) // "Hola "
