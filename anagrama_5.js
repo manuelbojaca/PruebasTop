a3e7f44aee665a5bfaff1de0dd00977a758ad66e
manueliciouss <manuel.bojaca@outlook.com>
2022-02-25 19:48:22 -0500

    completado 6 ejercicio leet code

--- anagrama_5.js
+++ anagrama_5.js
@@ -3,23 +3,50 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
     s = s + t;
     let cache = {};
-    let i = 0, j = 0;
-    s.split('').forEach(function (char){
-        if ((cache[char] && i >= t.length)) {
-            cache[char]--;
-        }else if(i >= t.length){
-            return false;
+    for (let i = 0; i <= s.length-1; i++){ 
+
+        if(i > t.length-1){
+    
+            if (cache[s[i]]) {
+                cache[s[i]]--;
+    
+            }else{
+                return false;
+            }
         }else{
+            cache[s[i]] = (cache[s[i]] || 0) + 1;
+            console.log('--INTO-else--');
+        }
+        console.log(cache);
+    }
+
+    return true;
+        
+};
+
+console.log(isAnagram('rat','car')); //false
+console.log(isAnagram('casa','tara')); //false
+console.log(isAnagram('cocodrilo','lodricoco')); //true
+console.log(isAnagram('coloridosnjh','ksahuhasdñuhds')); //false
+
+/*
+console.log('BEGIN i; ', i, ' t: ', t.length);
+        
+        if(i > t.length-1){
+            
+            console.log('--INTO-if-- cache:', cache[char], ' char: ', char);
+            if (cache[char]) {
+                cache[char]--;
+                console.log('--INTO--');
+            }else{
+                console.log('--INTO-false--');
+                return false;
+            }
+        }else{
+            console.log('--INTO-else--');
             cache[char] = (cache[char] || 0) + 1;
         }
         i++;
-    });
-    return true;
-};
-
-console.log(isAnagram('rat','tar'));
-console.log(isAnagram('casa','tar'));
-console.log(isAnagram('cocodrilo','lodricoco'));
-console.log(isAnagram('coloridosnjh','ksahuhasdñuhds'));
-
+        console.log(cache);
     
+*/
---
+++ ceroRight_6.js
@@ -0,0 +1,12 @@
+
+var moveZeroes = function(nums) {
+    for (let last = 0, i = 0; i < nums.length; i++){
+        if(nums[i] !== 0) {
+            [nums[last++], nums[i]] = [nums[i], nums[last]];
+        }
+    }
+    return nums; 
+};
+
+console.log(moveZeroes([0,1,0,3,12])); //[1,3,12,0,0]
+console.log(moveZeroes([0])); //[0]
